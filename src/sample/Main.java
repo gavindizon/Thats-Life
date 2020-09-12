9d0b003edc5259c377862bf6acf9103d745982d1
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-09-12 22:52:26 +0800

    Updated

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        primaryStage.setTitle("That's Life");
+        primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
 
