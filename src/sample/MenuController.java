3a39edb3f7033ad3c08cbd8e47e1ab73f5e56eae
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-09-13 21:54:33 +0800

    Shitbag

--- src/sample/MenuController.java
+++ src/sample/MenuController.java
@@ -1,4 +1,48 @@
 package sample;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 
 public class MenuController {
+
+    public Button opt0;
+    public Button opt1;
+    public Button opt2;
+    // location and resources will be automatically injected by the FXML loader
+    @FXML
+    private URL location;
+
+    @FXML
+    private ResourceBundle resources;
+
+    public MenuController(){}
+
+    @FXML
+    private void initialize()
+    {
+    }
+
+
+    @FXML
+    public void menuOptions(ActionEvent e){
+        Stage stage = (Stage) opt2.getScene().getWindow();
+        if(e.getTarget() == opt0)
+            initNames(2);
+        else if(e.getTarget() == opt1)
+            initNames(3);
+        else
+            stage.close();
+    }
+
+    private void initNames(int numOfPlayers){
+        //fxmlloader
+        
+
+    }
+
+
 }
